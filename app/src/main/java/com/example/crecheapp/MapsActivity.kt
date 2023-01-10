6c3664a32ea1f9fe86a092e2912e7cae92fbefa0
Lucas Martins <alucas057@gmail.com>
2023-01-09 22:06:30 -0300

    subindo aula larissa

--- app/src/main/java/com/example/crecheapp/MapsActivity.kt
+++ app/src/main/java/com/example/crecheapp/MapsActivity.kt
@@ -40,7 +40,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        val sydney = LatLng(-8.1125809, -34.9210395)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
